dff100d7d666b6369404d379a459d9d44caf894b
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-13 17:12:33 +0000

    populate question and answer objects

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// Q + A obj
+
+var questions =  {
+    1 : "Commonly used data types DO NOT include:",
+    2 : "The condition in an if /else statement is enclosed within ____.",
+    3 : "Arrays in JavaScript can be used to store _____.",
+    4 : "String values must be enclosed within _____ when being assigned to variables.",
+    5 : "A very useful tool used during development and debugging for printing content to the debugger is:"
+}
+
+var correctAnswers = {
+    1 : "alerts",
+    2 : "parentheses",
+    3 : "all of the above",
+    4 : "quotes",
+    5 : "console.log"
+};
+
+var wrongAnswers = {
+    1 : ["strings", "booleans", "numbers"],
+    2 : ["quotes", "curly brackets", "square brackets"],
+    3 : ["numbers and strings", "other arrays", "booleans"],
+    4 : ["commas", "curly brackets", "parentheses"],
+    5 : ["JavaScript", "terminal / bash", "for loops"]
+}
