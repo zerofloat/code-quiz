7af3f21a476d120535261d38639372ed4923b61a
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-15 14:50:52 +0000

    replace objs with arrays and fix button last child display styling

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,7 +5,8 @@ var questionScreen = document.getElementById("questions");
 var startButton = document.getElementById("start");
 var choices = document.getElementById("choices");
 // placeholder:
-var questionNum = 2;
+var answerEl = "";
+var questionNum = 4;
 
 function startQuiz() {
     startScreen.classList.replace("start", "hide");
@@ -15,23 +16,31 @@ function startQuiz() {
 function quizGameplay() {
     questionScreen.classList.toggle("hide");
     renderChoices(questionNum);
+    // answerEl.addEventListener("click", answerCompare);       
+}
 
-    
-    }
     // remember to add questionNum args to function and for answerChoices [index]
 function renderChoices(questionNum) {
         for (const key in answerChoices) {
             const choice = answerChoices[questionNum];
-            console.log(choice[key]); 
+            // console.log(choice[key]); 
             var answerEl = document.createElement("button");
             answerEl.textContent = choice[key];
-            choices.appendChild(answerEl);           
+            choices.appendChild(answerEl);
+            console.log(answerEl);
             }
-        }
+}
+
+
         
-function name(params) {
-    
-}    
+function answerCompare(key) {
+    if (condition) {
+        
+    }
+}
+
+
+ 
         
         // const choices = document.getElementById("choices");
         // const answerEl = document.createElement("li");
