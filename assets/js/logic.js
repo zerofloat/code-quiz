a8063f4738ce8c8e776d930d1628d1d99abb822a
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-15 20:19:34 +0000

    add event listener for button parent element

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -5,23 +5,39 @@ var questionScreen = document.getElementById("questions");
 var startButton = document.getElementById("start");
 var choices = document.getElementById("choices");
 var questionEl = document.getElementById("question-title");
+// var answerButtons = document.querySelectorAll("c");
 
 // placeholders:
 var answerEl = "";
-var questionNum = 1;
+var questionNum = 0;
 
 function startQuiz() {
     startScreen.classList.replace("start", "hide");
     quizGameplay();
 }
 
-function quizGameplay() {
-    questionScreen.classList.toggle("hide");
-    renderChoices();
-    // answerEl.addEventListener("click", answerCompare);       
-}
 
-    // remember to add questionNum args to function and for answerChoices [index]
+
+
+
+// // https://byby.dev/js-add-event-listener
+//     answerButtons.forEach(function(button) {
+//         button.addEventListener("click", function() {
+//     // do something when the button is clicked
+//             console.log("You clicked a button");
+//   });
+// });
+
+
+
+// answerButtons.forEach(function(button){
+        // button.addEventListener("click", )
+    // answerButtons.addEventListener("click", answerCompare);       
+
+
+
+
+    // use questionNum counter to increment functions
 function renderChoices() {
         for (const key in answerChoices) {
             const questch = questions[questionNum];
@@ -31,20 +47,36 @@ function renderChoices() {
             answerEl = document.createElement("button");
             answerEl.textContent = choice[key];
             choices.appendChild(answerEl);
-            console.log(answerEl);
+            // console.log(answerEl);
             }
 }
 
 
-        
-function answerCompare(key) {
-    if (condition) {
+function quizGameplay() {
+    questionScreen.classList.toggle("hide");
+    renderChoices();
+// https://blog.bitsrc.io/cleaner-code-with-event-delegation-in-javascript-bb6dd27be64d
+// https://stackoverflow.com/questions/73593129/js-extract-button-text-content-on-click
+    choices.addEventListener("click", function(event) {
+        const answerBtn = event.target.closest("button");
+        if (answerBtn.textContent === answersCorrect[questionNum]) {
+            
+        }
+    }
+)
+}
+    
 
+        
+function answerCompare() {
+    console.log(test);
+    // if (condition) {
+        
     }
 
     questionNum++;
     // renderChoices();
-}
+
 
 
 
