3a0f1f57f8bb1337f822a80dd65be18add88fc76
zerofloat <133889261+zerofloat@users.noreply.github.com>
2024-01-17 17:16:47 +0000

    add additional scores vars

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,11 +1,22 @@
 var initialsInput = document.getElementById("initials");
 var highScoresList = document.getElementById("highscores");
 var highScores = [];
-var score = initialsInput.value.trim();
+var scoreLi = document.createElement("li");
+var submitButton = document.getElementById("submit");
+
+
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var score = initialsInput.value.trim();
+    
+})
+
+
+
 
 function renderHighScores() {
 // highScoresList.innerHTML = "";
-var scoreLi = document.createElement("li");
 scoreLi.textContent = score;
 
 
